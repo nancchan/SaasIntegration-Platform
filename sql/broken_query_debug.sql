13c38f81bd023c03c60c66c772b3a5eec4b40b7e
nancchan <nancy.chang3887@gmail.com>
2026-06-07 22:48:03 -0700

    Delete sql/broken_query_debug.sql

--- sql/broken_query_debug.sql
+++
@@ -1,7 +0,0 @@
--- INCIDENT: SQL query failure due to syntax error in reporting system
--- IMPACT: Blocked investigation into customer dataset issues
--- PURPOSE: Demonstrate debugging of broken SQL query
-
-SELECT name, email
-FROM customers
-WHERE customer_id IS NOT NULL;
