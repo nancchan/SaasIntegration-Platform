755413f967a2e52d44a5a476a86c99a9d49deb78
nancchan <nancy.chang3887@gmail.com>
2026-07-09 22:48:29 -0700

    Delete sql/orders_per_customer_report.sql

--- sql/orders_per_customer_report.sql
+++
@@ -1,9 +0,0 @@
--- INCIDENT: Inconsistent customer activity metrics in dashboard
--- IMPACT: Incorrect segmentation of active vs inactive customers
--- PURPOSE: Aggregate order volume per customer
-
-SELECT
-    customer_id,
-    COUNT(*) AS total_orders
-FROM orders
-GROUP BY customer_id;
