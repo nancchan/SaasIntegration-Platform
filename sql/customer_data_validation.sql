68798d0944ea45605fa4d7528de1c5c6e73558fd
nancchan <nancy.chang3887@gmail.com>
2026-07-09 22:46:27 -0700

    Update and rename customer_data_validation.sql to customer_lookup.sql

--- sql/customer_data_validation.sql
+++
@@ -1,9 +0,0 @@
--- INCIDENT: CRM sync failures due to missing or invalid customer data
--- IMPACT: 18–30% of customer onboarding records failed CRM sync
--- PURPOSE: Identify records that will be rejected by CRM validation
-
-SELECT *
-FROM customers
-WHERE email IS NULL
-   OR email = ''
-   OR phone IS NULL;
